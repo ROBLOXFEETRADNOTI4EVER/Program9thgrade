0985175a45f157c39b35216995a3997ae6c4350e
THEFFEETYMIGHTYMOO <108127820+ROBLOXFEETRADNOTI4EVER@users.noreply.github.com>
2024-09-03 20:56:52 +0200

    Add files via upload

---
+++ #lists.py
@@ -0,0 +1,32 @@
+#lists
+
+list1 = []
+
+list2 = [1,3,6,3,9]
+
+list3= ["John",23,56.0,True]
+
+list4 = [3 + 5 , False]
+
+list5 = [4 + 5 , "Jhons"]
+
+BBCLISTSUUKKA = ["HUZD RA CIGANY", "HUZD RA MEGYEGY CIGANY",4 *2 + 3]
+
+print(BBCLISTSUUKKA)
+
+
+nyames = ["BBC","BBC1","BBC2", "BBC3", "BBC4"]
+print(nyames)
+print(nyames[0])
+print(nyames[3])
+
+
+marks = [56,76,69,71,39]
+marks[4] = 43
+
+print(marks)
+
+names = ["Iijsi","Sdijijs","SDDSDS","ososkosk"]
+print(names)
+del names[1]
+print(names)
