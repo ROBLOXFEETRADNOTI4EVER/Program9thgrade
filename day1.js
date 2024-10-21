f4a2c49187a43b9150f1c24f2cd99be606a76724
THEFFEETYMIGHTYMOO <108127820+ROBLOXFEETRADNOTI4EVER@users.noreply.github.com>
2024-10-21 17:52:42 +0200

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,97 @@
+
+
+
+// i can make notes like this in js and C and C++
+
+
+// console.log('Hello'); // to log something on the console
+// console.log("I hate nigger");  // to log something on the console
+
+// // window.alert('I am an alert'); // to creat an alert
+// // window.alert('I hate niggers'); // to creat an alert
+
+// // this is a command
+
+// /*
+// this is a 
+// comment
+// */
+
+
+// document.getElementById('Hello').textContent = 'Hello World';
+// document.getElementById('MyPenis').textContent = 'I hate niggers';
+
+
+// let x = 100;
+// console.log(x);
+// let y;
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+// console.log(typeof price);
+// console.log(`You are ${age} years old`); // ${} is like f strings in python
+// console.log(`the price is $${price}`);
+// console.log(`Your gpa is: ${gpa}`);
+
+
+
+// let firstName = `Nigger`
+// let favFod = 'KFC+Watermelon'
+// let email = 'nigger1@gmail.com'; // can't use string from the nigger 1 to do maath
+
+// console.log(`your name is ${firstName}`);
+// console.log(`you like child and your favorite food is: ${favFod}`);
+// console.log(`Your email is ${email}`)
+
+// let gender = false; console.log(`${gender}`)
+// console.log(typeof gender)
+
+
+
+// if (gender === false){ // we use 3 to check type and value
+//     console.log(`You are a woman`)
+// }
+
+// else if (gender === true){
+//     console.log(`You are a man`)
+// }
+
+
+// let students = 30;
+
+// students = students + 1; // to add 1 to the current like in python +=
+// students = students - 1; // to remove 1 like in python
+// students = students * 2; // tis multiplys
+//students = students / 2; // this bassicly devides students by 2 
+//students = students **2; // this gives the powerr of 2 so négyzet in hungaarian
+// let Extrastudents = students % 3; // modulus  operator
+// console.log(students);
+
+// students += 1; // FUCKING EASIER THEN DOING THE WHOLE = AND = SHIT btw this gives + = 1 plush eaqucls 1
+// students -= 1; // FUCKING EASIER THEN DOING THE WHOLE = AND = SHIT btw this gives - = 1 minus eaqucls 1
+// students *= 2;
+//students /= 2; // this devides by 2
+//students **= 2; // this gives the powerr of 2 so négyzet in hungaarian
+// students %= 2; // modulus
+
+
+
+// students++; // this increments so it  only adds one its called increment operator
+// students--; // this called decrement operaator so it removes 1 from current
+
+// console.log(students);
+/*
+    Operator precedence
+    1 parathethis ()
+    2 exponents
+    3 multiplication & division & modulo
+    4 addition & substration
+
+    in hungarian Müveletek sorendje
+*/
+// let result = 1 + 2 * 3 + 4 ** 2;
+// let result =  12 % 5 + 8 / 2;
+// let result = 6 / 2 ** (2 + 5);
+
+// console.log(result);
