f370db67dad6ced7ba6dbc4de2eff7da5c01143b
THEFFEETYMIGHTYMOO <108127820+ROBLOXFEETRADNOTI4EVER@users.noreply.github.com>
2024-10-23 21:21:16 +0200

    Add files via upload

--- day1.js
+++ day1.js
@@ -151,3 +151,28 @@
 //     document.getElementById("myH3").innerHTML = circumference
 
 // }
+
+const decraseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+decraseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
