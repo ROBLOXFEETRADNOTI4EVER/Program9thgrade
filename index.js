08ff2b24c4e8c6144aeb43416d08944b50828794
THEFFEETYMIGHTYMOO <108127820+ROBLOXFEETRADNOTI4EVER@users.noreply.github.com>
2024-10-31 00:27:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ let starConfigs = [];
 
 const mapToRange = (number, inMin, inMax, outMin, outMax) => {return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;}
 
-for(let i=0; i<100; i++){
+for(let i=0; i<2000; i++){
 	let size = mapToRange(Math.random(), 0,1, 10, 30);
 	let config = {
-		fill:`hsl(${mapToRange(Math.random(), 0,.00011, 110, 60)}, 600000%, 50%)`,
+		fill:`hsl(${mapToRange(Math.random(), 4,.00011, 1, 1110)}, 600000%, 50%)`,
 		numPoints: 5,
 		origin: {x:Math.random() * w, y:mapToRange(Math.random(), 0,1, -size, h)},
 		radiusInner: size * .6,
@@ -122,3 +122,66 @@ animate();
 
 // }
 // console.log(letterGrade);
+
+
+
+
+
+// let username = "dBaoloazsofffo";
+
+// console.log(username.charAt(0));
+// console.log(username.indexOf("o")); // it will return index of its currenct postion in rn at position 7
+
+// console.log(username.lastIndexOf("o")); it will log the last one of the selected item in the string 
+
+// console.log(username.length) // it will log out us the lenght of the current string 
+
+// username = username.trim();
+// console.log(username)
+
+// username = username.toUpperCase();
+// console.log(username);
+
+// username = username.toLocaleLowerCase();
+// console.log(username);
+
+// username = username.endsWith(""),("o");
+// console.log(username)
+
+// username = username.trim(username.repeat(3));
+
+// console.log(username);
+
+// // username = username.startsWith(" ");
+// // console.log(username)
+// let result = username.startsWith(" ");
+
+// if(result){
+// 	console.log("You are gay");
+// }
+// else{
+// 	console.log("You are not gay");
+// }
+
+
+// let result = username.includes(" ");
+
+// if(result){
+// 	console.log("Your username can't incliude ' ' Spaces");
+
+// }
+// else{
+// 	console.log(`Your username seems fine ${username}`)
+// }
+
+
+let phonenumber = "123-335-32325";
+
+// phonenumber = phonenumber.replaceAll("-","");
+// console.log(phonenumber);
+
+// phonenumber = phonenumber.padStart(30, "0"); // this fills the front of the string with the other string until the character char is 15 characters long 
+
+// phonenumber = phonenumber.padEnd(30, "0"); // this  fillls the ond of the string with the other string until the character char is 15 characters long 
+
+console.log(phonenumber)
