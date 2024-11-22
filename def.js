ca69c88caa30231965518136a7d6653d028ae355
THEFFEETYMIGHTYMOO <108127820+ROBLOXFEETRADNOTI4EVER@users.noreply.github.com>
2024-11-22 22:20:10 +0100

    Add files via upload

--- def.js
+++ def.js
@@ -84,6 +84,89 @@ console.log(F.join(""));
 
 function formatPamynets(element){
     const parts = element.split("-");
-    return `\nUser:${parts[2]} paid, ${parts[0]} $bucks,${parts[1]}<-- tis time`
+    return `\nUser:${parts[2]} paid, ${parts[0]} $bucks,${parts[1]}<-- tis time`;
 
 }
+
+
+
+const Immigrants = [
+    "Migrant1-NH-Terrorist",
+    "Migrant2-NH-GoatFucker",
+    "Migrant3-NY-Thief",
+    "Migrant4-CA-Scammer",
+    "Migrant5-TX-Smuggler",
+    "Migrant6-FL-Spy",
+    "Migrant7-IL-CyberCriminal",
+    "Migrant8-PA-Fraudster",
+    "Migrant9-OH-HumanTrafficker",
+    "Migrant10-MI-DrugDealer",
+    "Migrant11-GA-Vandal",
+    "Migrant12-NC-Burglar",
+    "Migrant13-NJ-Kidnapper",
+    "Migrant14-VA-Terrorist",
+    "Migrant15-WA-GoatFucker",
+    "Migrant16-AZ-Arsonist",
+    "Migrant17-MA-Thief",
+    "Migrant18-IN-Fraudster",
+    "Migrant19-TN-Smuggler",
+    "Migrant20-MO-CyberCriminal",
+    "Migrant21-MD-DrugDealer",
+    "Migrant22-WI-Spy",
+    "Migrant23-CO-Burglar",
+    "Migrant24-MN-HumanTrafficker",
+    "Migrant25-SC-Kidnapper",
+    "Migrant26-AL-Fraudster",
+    "Migrant27-LA-Arsonist",
+    "Migrant28-KY-Smuggler",
+    "Migrant29-OR-Thief",
+    "Migrant30-OK-Terrorist",
+    "Migrant31-CT-Spy",
+    "Migrant32-IA-DrugDealer",
+    "Migrant33-MS-Burglar",
+    "Migrant34-AR-Vandal",
+    "Migrant35-NV-Kidnapper",
+    "Migrant36-NM-CyberCriminal",
+    "Migrant37-UT-HumanTrafficker",
+    "Migrant38-WV-Fraudster",
+    "Migrant39-HI-Smuggler",
+    "Migrant40-ID-Arsonist",
+    "Migrant41-ME-Thief",
+    "Migrant42-MT-Vandal"
+];
+
+const Muslims = Immigrants.map(GoatFuckers);
+console.log(Muslims.join("|-xx-|"));
+
+function GoatFuckers(element){
+    const  parts = element.split("-");
+    return `\nPeople :${parts[0]} Wantedfor: ${parts[2]} County${parts[1]}`;
+}
+
+
+// under me i will do Name religion Budget and pat 
+
+const Niggers = ["Bob|Chirstian|50$|Dog","Sam|Atheist|90$|None","smith|Budhism|44$|Parrot","spongabob|Muslim|84$|Sark"];
+
+
+const niggermigger = Niggers.map(DeportNigger);
+console.log(niggermigger.join("|here|"))
+
+
+function DeportNigger(element){
+    const Niglets = element.split("|");
+    return `Name ${Niglets[0]} Pet ${Niglets[3]} Religion ${Niglets[1]}, Money ${Niglets[2]}`;
+    
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const checkbox = document.getElementById('checkboxItself');
+
+    // Add an event listener for the 'change' event
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            // Redirect to scam.html when the checkbox is checked
+            window.location.href = 'scam.html';
+        }
+    });
+});
