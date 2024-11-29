fdb72873de3c7598075c018307317080b06eaf80
THEFFEETYMIGHTYMOO <108127820+ROBLOXFEETRADNOTI4EVER@users.noreply.github.com>
2024-11-29 12:03:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,168 +1,44 @@
-// creating a basic calorie counter in jacscript
-// creat an input to enter foods with each gram of protein carbs and fat and optional waterweight of it so it can be calculated 
-// after it show what is avalable in  a list format can do dropdown aswell
-// make user select or type what he  ate and then calculate the calroies
-// allso creat a function so user can select calaorie each day max and min and program upates real time to it if its over or under
-// optional user can select min and max of each protein carbs and fatsb but can't exceed the calrie limit 
-// also make it so it won't just ask for 100g  since everything is  in 100 grams so i need to divide it by 10 
-
-let calroiesMax = 2000;
-let calroiesMin = 1250;
-let userCalories = 0;
-let calorie = 0;
-let userCarsbs = 0;
-let userProteins = 0;
-let userFats = 0;
-let selectedFoods = document.getElementById("somth")
-selectedFoods.innerHTML = "" 
-
-const carbs = 4;
-const proteins = 4;
-const fats = 9;
-
-// let food100g = [
-//     "orange|12|0.9|0.2", 
-//     "milk|5|3.4|3.3",    
-//     "beef|0|26|15",      
-//     "eggs|0.6|13|10"     
-// ];// strcture is like this : Name|Carbs|Proteins|Fats
-
-// let showitems = document.getElementById(`searchYeah`)
-
-// let foods = food100g.map(food => food.split("|")[1]); // [to do[] make it so i can divide the carbs protiens and fats
-
-
-// console.log(foods);
-
-// okay i gonna do that i will put a picture and console log the carsb a person can eat in a day
-
-calorie = (userCarsbs + userProteins + userFats)
-let calvis = document.getElementById("calVisual")
-calvis.innerHTML = `Calories ${calorie} \n Carbs ${userCarsbs}\n Proteins ${userProteins}\n fats ${userFats}`
-
-
-let egg = document.querySelector("Egg");
-
-
-
-function eggmae(){
-    selectedFoods.innerHTML = "Egg" ;
-    eggadd();
-
-}
-
-
-let Lunclyy = document.querySelector("Lunclly");
-function Luncly(){
-    selectedFoods.innerHTML = "luncly"
-    Lunclyy1()
-}
-
-function eggadd() {
-    // Update the user's macros
-    userCarsbs += 0.5; // Add carbs for the egg
-    userProteins += 6; // Add proteins for the egg
-    userFats += 6; // Add fats for the egg
-
-    // Calculate the updated calories dynamically
-    userCalories = (userCarsbs * carbs) + (userProteins * proteins) + (userFats * fats);
-    // Update the visual representation
-    let calvis = document.getElementById("calVisual");
-    calvis.innerHTML = `Calories: ${userCalories.toFixed(0)} <br> 
-                        Carbs: ${userCarsbs.toFixed(1)}g <br> 
-                        Proteins: ${userProteins.toFixed(1)}g <br> 
-                        Fats: ${userFats.toFixed(1)}g`;
-    // Optionally, log to the console for debugging
-    console.log("Egg added. Updated values:");
-    console.log(`Calories: ${userCalories}, Carbs: ${userCarsbs}, Proteins: ${userProteins}, Fats: ${userFats}`);
+function getInputValue() {
+    const input = document.querySelector("#myInput").value;
+    console.log(input); 
 }
 
 
 
-function Lunclyy1(){
-    userCarsbs += 22;
-    userProteins += 11;
-    userFats += 12;
-    userCalories = (userCarsbs * carbs) + (userProteins * proteins) + (userFats * fats);
-    let calvis = document.getElementById("calVisual");
-    calvis.innerHTML = `Calories: ${userCalories.toFixed(0)} <br> 
-                        Carbs: ${userCarsbs.toFixed(1)}g <br> 
-                        Proteins: ${userProteins.toFixed(1)}g <br> 
-                        Fats: ${userFats.toFixed(1)}g`;
-                        console.log("Lunchly added I got my drippy cheese. Updated values:");
-                        console.log(`Calories: ${userCalories}, Carbs: ${userCarsbs}, Proteins: ${userProteins}, Fats: ${userFats}`);
+// async function getData() {
+//     const url = " http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={e19c42b615d07843c4780e7fc0987adf}";
+//     try {
+//       const response = await fetch(url);
+//       if (!response.ok) {
+//         throw new Error(`Response status: ${response.status}`);
+//       }
+  
+//       const json = await response.json();
+//       console.log(json);
+//     } catch (error) {
+//       console.error(error.message);
+//     }
+//   }
+// //   e19c42b615d07843c4780e7fc0987adf
 
-}
+// const prices = [5,30,10,25,15,20];
+// const total = prices.reduce(sum);
+// // console.log(total);
+// // it bassicly makes so all of the values in the array get added or summed together this feture is pretty usefull
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let numbs = [1,2,3,4,5,6,7,8,9];
-// let evenumbs = numbs.filter(Iseven);
-// let oddnumbs = numbs.filter(isodd);
-// console.log(`${evenumbs}`);
-// console.log(`${oddnumbs}`);
-
-// function Iseven(element){
-//     return  element % 2 === 0;
+// console.log(`$${total.toFixed(2)}`);
+// // we use tofixed to get the 2 decimals its fucking good and good feture must use in the future
+// function  sum(accumulator,element){
+//     return accumulator + element;
 // }
 
+// ------------------------------------------------------------------------------------------|
+// const grades = [4,3,2,4,5,3,2,4,1,2,3,2,1,3,4,5,5,2,4,3,2,4,2,2,1,3,1,3,5,6,2,1,4,5];     |
+// const avragGrades = grades.reduce(Avrage);                                                |
+// console.log(`${avragGrades.toFixed(2)}`);                                                 |
+//                                                                                           |
+// function Avrage(accumulator,element){                                                     |
+//     return (accumulator + element) / element;                                             |
+// }                                                                                         |
+// ------------------------------------------------------------------------------------------|
 
-
-// function isodd(element){
-//     return element % 2 === 1;
-
-// }
